c50df8a6b0a87064ed8929cd03296fdee15a411e
ricard1997 <ricar.d1997@hotmail.com>
2021-10-03 14:10:21 -0500

    Almost done

--- script.js
+++ script.js
@@ -108,3 +108,18 @@ window.addEventListener("load",() =>  {
 
   preloader.classList.add('preload-finish');
 });
+
+
+function mueveElBoton(){
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  newWidth = (Math.random() * width/2);
+  newHeight = (Math.random() * height/2);
+
+  document.getElementById('btnNo').style.position = "absolute";
+  document.getElementById('btnNo').style.left = newWidth + "px";
+  document.getElementById('btnNo').style.top = newHeight + "px";
+  
+
+}
